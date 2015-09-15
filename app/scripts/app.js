cd6e105de85feca66f06683d495c1aab5e052c45
Hoang Loi <hoangloi@Hoangs-MacBook-Pro.local>
2015-09-15 23:59:13 +0700

    push code

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name quikiePrototypeAdminApp
+ * @description
+ * # quikiePrototypeAdminApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('quikiePrototypeAdminApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'ui.bootstrap'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name quikiePrototypeAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the quikiePrototypeAdminApp
+ */
+angular.module('quikiePrototypeAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
